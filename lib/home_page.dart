2b26e64979607db079ebc45fa59a5dfd2b31efe1
Gtt <vijaymobilesoftware@gmail.com>
2021-11-03 16:28:19 +0530

    Day2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  // variable declaration with data type explicit
+  int days = 30;
+  String name = "Vijaygdg ";
+  double pi = 3.14;
+  bool isMale = true;
+  num temp = 30.5;
+  //variable declaration with data type implicit
+  var day = "Tuesday";
+  // const piVal = 3.14;
+  final vari = 10;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Cataloge App"),
+      ),
+      body: Center(
+        child: Text(
+            "Days $days name $name pi value is $pi is male $isMale today tem $temp and day is "),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_demo/home_page.dart';
+import 'package:flutter_demo/home_page.dart';
 
-void main()
-{
+void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // variable declaration with data type explicit
-    int days=30;
-    String name="Vijay";
-    double pi=3.14;
-    bool isMale=true;
-    num temp=30.5;
-    //variable declaration with data type implicit
-    var day="Tuesday";
-    const piVal=3.14;
-    piVal+1;
-
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child:Text("Days $days name $name pi value is $pi is male $isMale today tem $temp and day is $piVal"),
-          ),
-        ),
-    ),
+      home: HomePage(),
     );
   }
 }
